0e7ce10c17fcb210edb9e1c3496b15e3565a0d5b
mskim9967 <mskim9967@gmail.com>
2022-02-26 20:55:03 +0900

    update) ui

--- frontend/src/component/CustomPlaylistEdit.js
+++ frontend/src/component/CustomPlaylistEdit.js
@@ -90,142 +90,7 @@ function CustomPlaylistEdit({ setModalActive, playlistControl, music, lang, isDa
                       {`${playlist.name} (${playlist.data.length})`}
                       {idx === openIdx ? <IoChevronUp style={{ marginLeft: '5px' }} /> : <IoChevronDown style={{ marginLeft: '5px' }} />}
                     </div>
-                    <Modal
-                      autoMargin
-                      noPadding
-                      closeButton
-                      open={downloadKeyModa}
-                      onClose={() => setDownloadKeyModal(false)}
-                      width='280px'
-                      css={{ backgroundColor: isDark ? '#1c1c1c' : '#ffffff' }}
-                    >
-                      <div
-                        style={{ color: color.textBlack, padding: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 18 }}
-                      >
-                        <div style={{ fontSize: '16px', fontWeight: '500', wordBreak: 'keep-all', marginBottom: '3px' }}>
-                          {
-                            {
-                              kor: '아래에 키를 입력하여 플레이리스트를 가져옵니다',
-                              jpn: '以下にキーを入力してプレイリストを取得します',
-                              eng: 'Enter the key below to get the playlist',
-                            }[lang]
-                          }
-                        </div>
-                        <Input width='200px' placeholder='Key' onChange={(e) => setDownloadKey(e.target.value)} />
-                        <div style={{ height: '40px', display: 'flex', alignItems: 'center' }}>
-                          {isLoading ? (
-                            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                              <Loading type='points' color='error' size='lg' />
-                            </div>
-                          ) : (
-                            <Button
-                              css={{ fontSize: '15px' }}
-                              color='error'
-                              disabled={downloadKey?.length !== 6}
-                              auto
-                              onClick={async () => {
-                                setLoading(true);
-                                const res = await axiosInstance.get(`/playlist/${downloadKey}`, { validateStatus: false });
-                                if (!res.data.data) {
-                                  alert({ kor: '키가 유효하지 않습니다', jpn: '鍵が有効ではありません', eng: 'Key is not valid' }[lang]);
-                                  setLoading(false);
-                                  return;
-                                }
-                                let data = [];
-                                let ids = res.data.data.value.split(',');
-                                console.log(ids);
-                                for (let id of ids) {
-                                  const musicRes = await axiosInstance.get(`/music/${id}`);
-                                  data.push(musicRes.data.data);
-                                }
-                                let name = '';
-                                while (name !== null) {
-                                  name = prompt(
-                                    { kor: '새 플레이리스트 이름', jpn: 'プレイリス新しいプレイリストの名前', eng: "New playlist's name" }[lang]
-                                  );
-                                  if (name !== null) {
-                                    name = name.trim();
-                                    if (!name || name.length === 0)
-                                      alert({ kor: '이름이 비어있습니다', jpn: '名前があいています', eng: 'The name is empty' }[lang]);
-                                    else if (name.length > 10)
-                                      alert({ kor: '이름이 너무 깁니다', jpn: '名前があいています', eng: 'The name is empty' }[lang]);
-                                    else if (customPlaylist.find((e) => e.name === name))
-                                      alert({ kor: '이름이 중복됩니다', jpn: '名前が長すぎます', eng: 'The name too long' }[lang]);
-                                    else {
-                                      setCustomPlaylist([...customPlaylist, { name, data }]);
-                                      setDownloadKeyModal(false);
-                                      break;
-                                    }
-                                  }
-                                }
-                                setLoading(false);
-                              }}
-                            >
-                              {{ kor: '가져오기', eng: 'Import', jpn: '取り込み' }[lang]}
-                            </Button>
-                          )}
-                        </div>
-                      </div>
-                    </Modal>
-                    <Modal
-                      autoMargin
-                      noPadding
-                      closeButton
-                      open={keyModal}
-                      onClose={() => setKeyModal(false)}
-                      width='280px'
-                      css={{ backgroundColor: isDark ? '#1c1c1c' : '#ffffff' }}
-                    >
-                      <div
-                        style={{ color: color.textBlack, padding: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}
-                      >
-                        <div style={{ fontSize: '16px', fontWeight: '500', wordBreak: 'keep-all' }}>
-                          {
-                            {
-                              kor: '아래의 키 값을 다른 브라우저에 입력해주세요',
-                              jpn: '以下のキー値を他のブラウザに入力してください',
-                              eng: 'Please enter the KEY below in another browser',
-                            }[lang]
-                          }
-                        </div>
-                        <div
-                          style={{
-                            width: '200px',
-                            height: '50px',
-                            borderRadius: '20px',
-                            backgroundColor: color.lightGray,
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            color: color.textBlack,
-                            fontWeight: '600',
-                            fontSize: '24px',
-                            position: 'relative',
-                          }}
-                          onClick={() => {
-                            navigator.clipboard.writeText(key);
-                          }}
-                        >
-                          {key}
-                          <div style={{ position: 'absolute', right: 14, marginTop: '5px' }}>
-                            <IoClipboardOutline
-                              onClick={() => {
-                                navigator.clipboard.writeText(key);
-                              }}
-                            />
-                          </div>
-                        </div>
-                        <div style={{ fontSize: '14px', fontWeight: '400', opacity: '65%' }}>
-                          {
-                            {
-                              kor: '키는 3일간 유효합니다',
-                              jpn: 'キーは3日間有効です',
-                              eng: 'the key is valid for 3 days',
-                            }[lang]
-                          }
-                        </div>
-                      </div>
-                    </Modal>
+
                     <div style={{ heght: '100%', display: 'flex', alignItems: 'center' }}>
                       <Button
                         style={{ height: '90%' }}
@@ -336,6 +201,136 @@ function CustomPlaylistEdit({ setModalActive, playlistControl, music, lang, isDa
           </div>
         )}
       </div>
+
+      <Modal
+        autoMargin
+        noPadding
+        closeButton
+        open={downloadKeyModa}
+        onClose={() => setDownloadKeyModal(false)}
+        width='280px'
+        css={{ backgroundColor: isDark ? '#1c1c1c' : '#ffffff' }}
+      >
+        <div style={{ color: color.textBlack, padding: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 18 }}>
+          <div style={{ fontSize: '16px', fontWeight: '500', wordBreak: 'keep-all', marginBottom: '3px' }}>
+            {
+              {
+                kor: '아래에 키를 입력하여 플레이리스트를 가져옵니다',
+                jpn: '以下にキーを入力してプレイリストを取得します',
+                eng: 'Enter the key below to get the playlist',
+              }[lang]
+            }
+          </div>
+          <Input width='200px' placeholder='Key' onChange={(e) => setDownloadKey(e.target.value)} />
+          <div style={{ height: '40px', display: 'flex', alignItems: 'center' }}>
+            {isLoading ? (
+              <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <Loading type='points' color='error' size='lg' />
+              </div>
+            ) : (
+              <Button
+                css={{ fontSize: '15px' }}
+                color='error'
+                disabled={downloadKey?.length !== 6}
+                auto
+                onClick={async () => {
+                  setLoading(true);
+                  const res = await axiosInstance.get(`/playlist/${downloadKey}`, { validateStatus: false });
+                  if (!res.data.data) {
+                    alert({ kor: '키가 유효하지 않습니다', jpn: '鍵が有効ではありません', eng: 'Key is not valid' }[lang]);
+                    setLoading(false);
+                    return;
+                  }
+                  let data = [];
+                  let ids = res.data.data.value.split(',');
+                  console.log(ids);
+                  for (let id of ids) {
+                    const musicRes = await axiosInstance.get(`/music/${id}`);
+                    data.push(musicRes.data.data);
+                  }
+                  let name = '';
+                  while (name !== null) {
+                    name = prompt({ kor: '새 플레이리스트 이름', jpn: 'プレイリス新しいプレイリストの名前', eng: "New playlist's name" }[lang]);
+                    if (name !== null) {
+                      name = name.trim();
+                      if (!name || name.length === 0)
+                        alert({ kor: '이름이 비어있습니다', jpn: '名前があいています', eng: 'The name is empty' }[lang]);
+                      else if (name.length > 10) alert({ kor: '이름이 너무 깁니다', jpn: '名前があいています', eng: 'The name is empty' }[lang]);
+                      else if (customPlaylist.find((e) => e.name === name))
+                        alert({ kor: '이름이 중복됩니다', jpn: '名前が長すぎます', eng: 'The name too long' }[lang]);
+                      else {
+                        setCustomPlaylist([...customPlaylist, { name, data }]);
+                        setDownloadKeyModal(false);
+                        break;
+                      }
+                    }
+                  }
+                  setLoading(false);
+                }}
+              >
+                {{ kor: '가져오기', eng: 'Import', jpn: '取り込み' }[lang]}
+              </Button>
+            )}
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        autoMargin
+        noPadding
+        closeButton
+        open={keyModal}
+        onClose={() => setKeyModal(false)}
+        width='280px'
+        css={{ backgroundColor: isDark ? '#1c1c1c' : '#ffffff' }}
+      >
+        <div style={{ color: color.textBlack, padding: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}>
+          <div style={{ fontSize: '16px', fontWeight: '500', wordBreak: 'keep-all' }}>
+            {
+              {
+                kor: '아래의 키 값을 다른 브라우저에 입력해주세요',
+                jpn: '以下のキー値を他のブラウザに入力してください',
+                eng: 'Please enter the KEY below in another browser',
+              }[lang]
+            }
+          </div>
+          <div
+            style={{
+              width: '200px',
+              height: '50px',
+              borderRadius: '20px',
+              backgroundColor: color.lightGray,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: color.textBlack,
+              fontWeight: '600',
+              fontSize: '24px',
+              position: 'relative',
+            }}
+            onClick={() => {
+              navigator.clipboard.writeText(key);
+            }}
+          >
+            {key}
+            <div style={{ position: 'absolute', right: 14, marginTop: '5px' }}>
+              <IoClipboardOutline
+                onClick={() => {
+                  navigator.clipboard.writeText(key);
+                }}
+              />
+            </div>
+          </div>
+          <div style={{ fontSize: '14px', fontWeight: '400', opacity: '65%' }}>
+            {
+              {
+                kor: '키는 3일간 유효합니다',
+                jpn: 'キーは3日間有効です',
+                eng: 'the key is valid for 3 days',
+              }[lang]
+            }
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
