aff5c264ea6df7c5c9102867d1e00544ba99df1a
mskim9967 <mskim9967@gmail.com>
2022-03-07 02:19:54 +0900

    fix) playnext bug

--- frontend/src/component/MusicPlay.js
+++ frontend/src/component/MusicPlay.js
@@ -153,7 +153,6 @@ function MusicPlay({
             audio.current.play();
           } else {
             audioControl.playNext();
-            audio.current.play();
           }
         }
       }, 250);
